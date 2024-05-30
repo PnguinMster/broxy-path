b88034c6a9bc70f4c8565f81ce0a3a3bdfd1100d
Pnguin <marco_ga@aol.com>
2024-05-29 23:00:37 -0400

    Added basic movement

--- main.lua
+++ main.lua
@@ -1,3 +1,26 @@
+_G.love = require("love")
+
+function love.load()
+	_G.player = {}
+	player.x = 400
+	player.y = 200
+	player.speed = 300
+end
+
+function love.update(dt)
+	if love.keyboard.isDown("right", "d") then
+		player.x = player.x + player.speed * dt
+	elseif love.keyboard.isDown("left", "a") then
+		player.x = player.x - player.speed * dt
+	end
+
+	if love.keyboard.isDown("down", "s") then
+		player.y = player.y + player.speed * dt
+	elseif love.keyboard.isDown("up", "w") then
+		player.y = player.y - player.speed * dt
+	end
+end
+
 function love.draw()
-	love.graphics.print("Hello World", 400, 300)
+	love.graphics.rectangle("fill", player.x, player.y, 30, 60)
 end
