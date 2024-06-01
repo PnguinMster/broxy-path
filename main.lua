9157b9b8faf8a9b08f8d11ff82e39b65a2f2300c
Pnguin <marco_ga@aol.com>
2024-05-31 22:43:58 -0400

    Implemented physics to player

--- main.lua
+++ main.lua
@@ -1,26 +1,33 @@
 _G.love = require("love")
 
 function love.load()
-	_G.player = {}
-	player.x = 400
-	player.y = 200
-	player.speed = 300
+	World = love.physics.newWorld(0, 30, true)
+
+	Player = {}
+	Player.x = 300
+	Player.y = 100
+	Player.width = 30
+	Player.height = 60
+	Player.body = love.physics.newBody(World, Player.x, Player.y, "dynamic")
+	Player.shape = love.physics.newRectangleShape(Player.width, Player.height)
+	Player.fixture = love.physics.newFixture(Player.body, Player.shape, 3)
+
+	Ground = {}
+
+	Ground.x = 300
+	Ground.y = 400
+	Ground.width = 90
+	Ground.height = 30
+	Ground.body = love.physics.newBody(World, Ground.x, Ground.y, "static")
+	Ground.shape = love.physics.newRectangleShape(Ground.width, Ground.height)
+	Ground.fixture = love.physics.newFixture(Ground.body, Ground.shape, 3)
 end
 
 function love.update(dt)
-	if love.keyboard.isDown("right", "d") then
-		player.x = player.x + player.speed * dt
-	elseif love.keyboard.isDown("left", "a") then
-		player.x = player.x - player.speed * dt
-	end
-
-	if love.keyboard.isDown("down", "s") then
-		player.y = player.y + player.speed * dt
-	elseif love.keyboard.isDown("up", "w") then
-		player.y = player.y - player.speed * dt
-	end
+	World:update(dt)
 end
 
 function love.draw()
-	love.graphics.rectangle("line", player.x, player.y, 30, 60)
+	love.graphics.rectangle("line", Player.x, Player.y, Player.width, Player.height, 3, 3)
+	love.graphics.rectangle("line", Ground.x, Ground.y, Ground.width, Ground.height)
 end
