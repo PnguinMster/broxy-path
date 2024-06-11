b734f26d55d0a350e5efc106e5f3999cf477b15f
Pnguin <marco_ga@aol.com>
2024-06-11 13:35:32 -0400

    Changed player movement

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 love = require("love")
 
+-- NOTE: NEW DIRECTION add toggle hover thing ability
+
 function love.load()
 	World = love.physics.newWorld(0, 9.81 * 15, true)
 
@@ -15,25 +17,29 @@ function love.load()
 	Player = {}
 	Player.width = 30
 	Player.height = 60
-	Player.start_x = (window_x / 2)
-	Player.start_y = (window_y / 2)
-	Player.body = love.physics.newBody(World, Player.start_x, Player.start_y, "dynamic")
-	Player.topShape = love.physics.newRectangleShape(0, Player.height / 4, Player.width, Player.height / 2)
-	Player.bottomShape = love.physics.newRectangleShape(0, -Player.height / 4, Player.width, Player.height / 2)
-	Player.topFixture = love.physics.newFixture(Player.body, Player.topShape, 2)
-	Player.bottomFixture = love.physics.newFixture(Player.body, Player.bottomShape, 2)
+	Player.startX = (window_x / 2)
+	Player.startY = (window_y / 2)
+	Player.topBody = love.physics.newBody(World, Player.startX, Player.startY, "dynamic")
+	Player.bottomBody = love.physics.newBody(World, Player.startX, Player.startY, "dynamic")
+	Player.topShape = love.physics.newRectangleShape(0, -Player.height / 4, Player.width, Player.height / 2)
+	Player.bottomShape = love.physics.newRectangleShape(0, Player.height / 4, Player.width, Player.height / 2)
+	Player.topFixture = love.physics.newFixture(Player.topBody, Player.topShape, 2)
+	Player.bottomFixture = love.physics.newFixture(Player.bottomBody, Player.bottomShape, 2)
+
+	Player.joint = love.physics.newWeldJoint(Player.topBody, Player.bottomBody, Player.startX, Player.startY, false)
+	Player.joint:setDampingRatio(1)
+	Player.joint:setFrequency(2)
 
 	Player.topFixture:setRestitution(0.2)
-	Player.topFixture:setFriction(0.3)
+	Player.topFixture:setFriction(0.7)
 	Player.bottomFixture:setRestitution(0.2)
 	Player.bottomFixture:setFriction(0.3)
-	Player.body:setAngularDamping(10)
+	Player.topBody:setAngularDamping(10)
+	Player.bottomBody:setAngularDamping(10)
 
 	Player.angular_force = 250
 	Player.topFixture:setCategory(LAYERS.PLAYER)
 	Player.bottomFixture:setCategory(LAYERS.PLAYER)
-	Player.topFixture:setMask(LAYERS.PLAYER)
-	Player.bottomFixture:setMask(LAYERS.PLAYER )
 
 	Ground = {}
 	Ground.width = 700
@@ -45,6 +51,7 @@ function love.load()
 	Ground.fixture = love.physics.newFixture(Ground.body, Ground.shape, 3)
 	Ground.body:setFixedRotation(true)
 	Ground.fixture:setCategory(LAYERS.LEVEL)
+	Ground.fixture:setFriction(0.7)
 
 	local r, g, b = love.math.colorFromBytes(25, 18, 28)
 	love.graphics.setBackgroundColor(r, g, b)
@@ -56,16 +63,23 @@ function love.update(dt)
 	World:update(dt)
 
 	if love.keyboard.isDown("right") then
-		Player.body:applyAngularImpulse(Player.angular_force)
+		Player.topBody:applyAngularImpulse(Player.angular_force)
+		Player.bottomBody:applyAngularImpulse(Player.angular_force)
 	elseif love.keyboard.isDown("left") then
-		Player.body:applyAngularImpulse(-Player.angular_force)
+		Player.topBody:applyAngularImpulse(-Player.angular_force)
+		Player.bottomBody:applyAngularImpulse(-Player.angular_force)
 	end
 end
 
 function love.draw()
-	local _, _, _, _, x3, y3, x4, y4 = Player.body:getWorldPoints(Player.topShape:getPoints())
-	local x1, y1, x2, y2 = Player.body:getWorldPoints(Player.bottomShape:getPoints())
+	-- TODO: Get Average from points
+	local x1, y1, x2, y2, x5, y5, x6, y6 = Player.topBody:getWorldPoints(Player.topShape:getPoints())
+	local _, _, _, _, x3, y3, x4, y4 = Player.bottomBody:getWorldPoints(Player.bottomShape:getPoints())
 	love.graphics.setColor(1, 0.5, 0)
-	love.graphics.polygon("line", x1, y1, x2, y2, x3, y3, x4, y4)
+
+	love.graphics.polygon("line", x1, y1, x2, y2, x5, y5, x6, y6, x4, y4, x3, y3, x5, y5, x6, y6)
+	-- love.graphics.polygon("line", x1, y1, x2, y2, x3, y3, x4, y4)
+	-- love.graphics.polygon("line", Player.topBody:getWorldPoints(Player.topShape:getPoints()))
+	-- love.graphics.polygon("line", Player.bottomBody:getWorldPoints(Player.bottomShape:getPoints()))
 	love.graphics.polygon("line", Ground.body:getWorldPoints(Ground.shape:getPoints()))
 end
