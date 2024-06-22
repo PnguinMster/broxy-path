222611a485d2b5d86c0b66c4123dd3772cf02068
Pnguin <marco_ga@aol.com>
2024-06-22 13:09:37 -0400

    added config file for love

---
+++ conf.lua
@@ -0,0 +1,17 @@
+function love.conf(t)
+	t.window.width = 750
+	t.window.height = 750
+	t.window.icon = nil
+	t.window.title = "Broxy Grath"
+	t.window.resizable = true
+	t.window.vsync = 0
+	t.window.minwidth = 400
+	t.window.minheight = 300
+
+	t.modules.data = false
+	t.modules.system = false
+	t.modules.touch = false
+	t.modules.video = false
+
+	t.version = "11.5"
+end
--- main.lua
+++ main.lua
@@ -8,31 +8,15 @@ local player = require("Player")
 
 function love.load()
 	love.graphics.setBackgroundColor(25 / 255, 18 / 255, 28 / 255)
-	love.window.setMode(750, 750, { resizable = true, vsync = 0, minwidth = 400, minheight = 300 })
-	-- love.window.setMode(750, 750)
-	-- love.window.setMode(750, 750, { vsync = 0 })
 
 	World = love.physics.newWorld(0, 9.8 * 16, true)
 
 	Player = player.new()
 
 	Tilemap:create_map(Tilemap.test_level, Player.width, Player.height)
-	-- Tilemap:optimize_map()
 	Tilemap:load_map()
 
 	Player:load()
-
-	-- Ground = {}
-	-- Ground.width = 1700
-	-- Ground.height = 30
-	-- Ground.start_x = (love.graphics.getWidth() / 2)
-	-- Ground.start_y = (love.graphics.getHeight() / 2) + 300
-	-- Ground.body = love.physics.newBody(World, Ground.start_x, Ground.start_y, "static")
-	-- Ground.shape = love.physics.newRectangleShape(Ground.width, Ground.height)
-	-- Ground.fixture = love.physics.newFixture(Ground.body, Ground.shape, 3)
-	-- Ground.body:setFixedRotation(true)
-	-- Ground.fixture:setCategory(LAYERS.LEVEL)
-	-- Ground.fixture:setFriction(0.7)
 end
 
 function love.update(dt)
@@ -53,6 +37,5 @@ function love.draw()
 	Tilemap:draw_map()
 	Player:draw()
 	love.graphics.setColor(1, 0.5, 0)
-	-- love.graphics.polygon("line", Ground.body:getWorldPoints(Ground.shape:getPoints()))
 	camera:unset()
 end
