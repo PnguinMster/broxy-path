1fe396eb5b81b8fdf3759abf62e23b829bdad7fd
Pnguin <marco_ga@aol.com>
2024-07-20 13:02:56 -0400

    added option menu

--- Scenes/OptionScene.lua
+++ Scenes/OptionScene.lua
@@ -1,45 +1,29 @@
 local button = require("UI.Button")
 local text = require("UI.Text")
 
--- NOTE: Menu Scene
-menu_scene = {}
-menu_scene.__index = menu_scene
-setmetatable({}, menu_scene)
+option_scene = {}
+option_scene.__index = option_scene
+setmetatable({}, option_scene)
 
 local RESIZE_DIFFERENCE = 5
 local window_x = 0
 local window_y = 0
 
-function menu_scene:load()
+function option_scene:load()
 	window_x = love.graphics.getWidth()
 	window_y = love.graphics.getHeight()
 
 	self.start_pressed = function()
 		print("Start button pressed")
-		Game:set_scene(SCENE.LEVEL)
 	end
 
-	self.options_pressed = function()
-		print("Options button pressed")
-		Game:set_scene(SCENE.OPTION)
-	end
-
-	self.quit_pressed = function()
-		print("Quit button pressed")
-		love.event.push("quit")
-	end
-
-	self.title_text = text.new("Broxy Grath", 3, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.TOP, 0, 10)
+	self.title_text = text.new("Options", 3, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.TOP, 0, 10)
 
 	self.start_button =
 		button.new(70, 50, "Start", self.start_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER)
-	self.options_button =
-		button.new(70, 50, "Options", self.options_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, 70)
-	self.quit_button =
-		button.new(70, 50, "Quit", self.quit_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, 140)
 end
 
-function menu_scene:update(dt)
+function option_scene:update(dt)
 	local x_difference = math.abs(window_x - love.graphics.getWidth())
 	local y_difference = math.abs(window_y - love.graphics.getHeight())
 
@@ -60,13 +44,13 @@ function menu_scene:update(dt)
 	end
 end
 
-function menu_scene:draw()
+function option_scene:draw()
 	self.title_text:draw()
 	self.start_button:draw()
 	self.options_button:draw()
 	self.quit_button:draw()
 end
 
-function menu_scene:unload()
+function option_scene:unload()
 	setmetatable(menu_scene, nil)
 end
--- Utility/SceneEnum.lua
+++ Utility/SceneEnum.lua
@@ -1,9 +1,11 @@
 require("Scenes.MenuScene")
 require("Scenes.LevelScene")
 require("Scenes.GameScene")
+require("Scenes.OptionScene")
 
 SCENE = {
 	MENU = menu_scene,
 	GAME = game_scene,
 	LEVEL = level_scene,
+	OPTION = option_scene,
 }
