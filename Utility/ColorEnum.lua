80ccfe7d54a31cadaa3f7898a9ea85e8bc6cd1cb
Pnguin <marco_ga@aol.com>
2024-09-13 17:38:01 -0400

    added sert colors

---
+++ Utility/ColorEnum.lua
@@ -0,0 +1,13 @@
+COLORS = {
+	LIME = { r = 168, g = 202, b = 88 }, -- Lime
+	WHITE = { r = 235, g = 237, b = 233 }, -- White
+	LIGHT_BLUE = { r = 115, g = 190, b = 211 }, -- Light Blue
+	DARK_BLUE = { r = 37, g = 58, b = 94 }, -- Dark Blue
+	ORANGE = { r = 218, g = 134, b = 62 }, -- Orange
+	RED = { r = 207, g = 87, b = 60 }, -- Red
+	MAROON = { r = 117, g = 36, b = 56 }, -- Maroon
+	PURPLE = { r = 122, g = 54, b = 123 }, -- Purple
+	PINK = { r = 223, g = 132, b = 165 }, -- Pink
+	GRAY = { r = 87, g = 114, b = 119 }, -- Gray
+	BLACK = { 25, 18, 28 },
+}
