984fef630d97291978587ddbc39bdf8b4f50575e
Pnguin <marco_ga@aol.com>
2024-12-30 21:59:08 -0500

    refactored unloading

--- UI/PauseMenu.lua
+++ UI/PauseMenu.lua
@@ -10,46 +10,38 @@ local RESIZE_DIFFERENCE = 5
 local window_x = 0
 local window_y = 0
 
+Pause_menu.interactables = {}
+Pause_menu.active = false
+
+local continue_pressed = function()
+	print("Continue pressed")
+	Pause_menu.active = false
+	Game:set_state(STATE.GAME)
+end
+local retry_pressed = function()
+	print("Retry pressed")
+	Game:set_state(STATE.GAME)
+	Pause_menu.active = false
+	Player:reset_player()
+end
+local menu_pressed = function()
+	print("Menu pressed")
+	Pause_menu.active = false
+	Game:set_scene(SCENE.MENU)
+end
+
 function Pause_menu:load()
 	window_x = love.graphics.getWidth()
 	window_y = love.graphics.getHeight()
 
-	self.interactables = {}
-
-	self.continue_pressed = function()
-		print("Continue pressed")
-		self.active = false
-		Game:set_state(STATE.GAME)
-	end
-	self.retry_pressed = function()
-		print("Retry pressed")
-		Game:set_state(STATE.GAME)
-		self.active = false
-		Player:reset_player()
-	end
-	self.menu_pressed = function()
-		print("Menu pressed")
-		self.active = false
-		Game:set_scene(SCENE.MENU)
-	end
-
 	self.title_text = text.new("Paused", 3, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.TOP, 0, 10, COLOR.WHITE)
 
-	self.continue_button = button.new(
-		85,
-		50,
-		"Continue",
-		self.continue_pressed,
-		nil,
-		HORIZONTAL_ALIGN.CENTER,
-		VERTICAL_ALIGN.CENTER,
-		0,
-		-140
-	)
 	self.interactables[1] =
-		button.new(85, 50, "Retry", self.retry_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, 0)
+		button.new(85, 50, "Continue", continue_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, -140)
 	self.interactables[2] =
-		button.new(85, 50, "Main Menu", self.menu_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, 140)
+		button.new(85, 50, "Retry", retry_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, 0)
+	self.interactables[3] =
+		button.new(85, 50, "Main Menu", menu_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, 140)
 end
 
 function Pause_menu:update(dt)
@@ -61,7 +53,6 @@ function Pause_menu:update(dt)
 		for _, interactable in ipairs(self.interactables) do
 			interactable.auto_resize_x()
 		end
-		self.continue_b:auto_resize_x()
 		window_x = love.graphics.getWidth()
 	end
 
@@ -86,5 +77,18 @@ function Pause_menu:draw()
 end
 
 function Pause_menu:unload()
-	setmetatable(Pause_menu, nil)
+	-- Title
+	self.title_text:unload()
+	self.title_text = nil
+
+	-- Unload interactables
+	for x, element in pairs(self.interactables) do
+		element:unload()
+		self.interactables[x] = nil
+	end
+
+	-- Unload rest of data
+	for k, _ in pairs(self) do
+		self[k] = nil
+	end
 end
