3ffa6fab455f30e927cba05ebd799cb58cedeb96
Pnguin <marco_ga@aol.com>
2025-01-14 20:17:21 -0500

    added hover sound

--- Scenes/SceneEvent.lua
+++ Scenes/SceneEvent.lua
@@ -1,7 +1,11 @@
 require("Utility.StateEnum")
 require("Utility.SceneEnum")
+require("Utility.SoundEffectEnum")
+require("Game.Sound")
 local pause_menu = require("UI.PauseMenu")
 local end_menu = require("UI.EndMenu")
+
+local hovered_ui = nil
 
 function love.keyreleased(key)
 	if Game.scene == SCENE.GAME then
@@ -22,16 +26,9 @@ function love.mousereleased(x, y, index)
 		local check_display = nil
 		check_display = Game.scene
 
-		if Game.scene == SCENE.GAME then
-			if pause_menu.active then
-				check_display = pause_menu
-			elseif end_menu.active then
-				check_display = end_menu
-			end
-		end
-
-		for _, interactable in ipairs(check_display.interactables) do
-			interactable:check_pressed(x, y)
+		if hovered_ui then
+			hovered_ui:check_pressed(x, y)
+			hovered_ui = nil
 		end
 
 		if check_display == SCENE.OPTION then
@@ -52,10 +49,38 @@ function love.mousepressed(x, y, index)
 	end
 end
 
-function love.mousemoved(x)
+function love.mousemoved(x, y)
 	if Game.scene == SCENE.OPTION then
 		for _, option_slider in ipairs(Game.scene.option_sliders) do
 			option_slider:mouse_moved(x)
+		end
+	end
+
+	local check_display = nil
+	check_display = Game.scene
+
+	if Game.scene == SCENE.GAME then
+		if pause_menu.active then
+			check_display = pause_menu
+		elseif end_menu.active then
+			check_display = end_menu
+		end
+	end
+
+	local ui_item = nil
+	for _, interactable in ipairs(check_display.interactables) do
+		ui_item = interactable:check_is_hovered(x, y)
+		if ui_item then
+			break
+		end
+	end
+
+	if ui_item ~= hovered_ui then
+		hovered_ui = ui_item
+		if hovered_ui then
+			--play sound
+			--change visual
+			Sound:play_sound_effect(SOUND_EFFECT.UI_HOVER)
 		end
 	end
 end
