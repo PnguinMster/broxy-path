6eb7c3047af00548c99b2c22bfae0f15da32368e
Pnguin <marco_ga@aol.com>
2025-01-15 19:59:44 -0500

    added comments

--- conf.lua
+++ conf.lua
@@ -1,21 +1,23 @@
 function love.conf(t)
+	--Window Configuration
+	t.window.title = "Broxy Grath"
+	t.window.icon = nil
 	t.window.width = 850
 	t.window.height = 750
-	t.window.icon = nil
-	t.window.title = "Broxy Grath"
+	t.window.minwidth = 400
+	t.window.minheight = 300
 	t.window.resizable = true
 	t.window.vsync = 0
 	t.window.msaa = 3
-	t.window.minwidth = 400
-	t.window.minheight = 300
 	t.window.display = 2
 
+	--Disable unused modules
 	t.modules.data = false
 	t.modules.system = false
 	t.modules.touch = false
 	t.modules.video = false
 
-	t.version = "11.5"
+	t.version = "11.5" --LOVE version
 
-	t.identity = "game_data"
+	t.identity = "game_data" --save directory name
 end
