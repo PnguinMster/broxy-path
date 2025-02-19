8fba6e09d33ce01a0364a0de075bdaa61da75aa3
Pnguin <marco_ga@aol.com>
2025-02-19 16:26:37 -0500

    changed test level order

--- Utility/LevelImagesEnum.lua
+++ Utility/LevelImagesEnum.lua
@@ -1,5 +1,4 @@
 LEVEL_IMAGES = {
-	test = "Art/Level/test_block_colored.png",
 	level_0 = "Art/Level/level_0.png",
 	level_1 = "Art/Level/level_1.png",
 	level_2 = "Art/Level/level_2.png",
@@ -9,4 +8,5 @@ LEVEL_IMAGES = {
 	level_6 = "Art/Level/level_6.png",
 	level_7 = "Art/Level/level_7.png",
 	level_8 = "Art/Level/level_8.png",
+	test = "Art/Level/test_block_colored.png",
 }
