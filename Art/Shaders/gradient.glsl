91acacfdbbebf825108644396b88b232f5dc6f54
Pnguin <marco_ga@aol.com>
2025-03-06 19:14:46 -0500

    tweaked

--- Art/Shaders/gradient.glsl
+++ Art/Shaders/gradient.glsl
@@ -3,6 +3,6 @@ extern vec4 end_color;
 extern vec2 direction;
 
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
-	float t = dot(texture_coords, direction);
-	return mix(start_color, end_color, t);
+    float t = dot(texture_coords, direction);
+    return mix(start_color, end_color, t);
 }
