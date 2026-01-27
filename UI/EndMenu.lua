a60dc51c79f98452742e15dac55b3c48bcff8073
Pnguin <marco_ga@aol.com>
2026-01-27 15:30:26 -0500

    updated end menu buttons size

--- UI/EndMenu.lua
+++ UI/EndMenu.lua
@@ -28,9 +28,9 @@ function End_menu:load()
 
 	--buttons
 	self.interactables[1] =
-		button.new(120, 63, "Retry", retry_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, -58)
+		button.new(135, 63, "Retry", retry_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, -58)
 	self.interactables[2] =
-		button.new(120, 63, "Main Menu", menu_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, 58)
+		button.new(231, 63, "Main Menu", menu_pressed, nil, HORIZONTAL_ALIGN.CENTER, VERTICAL_ALIGN.CENTER, 0, 58)
 end
 
 function End_menu:resize(width, height)
