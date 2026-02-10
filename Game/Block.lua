9eea5edb9dc834b5aee3ff582c79f96c1f8af9a1
Pnguin <marco_ga@aol.com>
2026-02-10 18:52:17 -0500

    changed sizing of end gate. smaller collision compared to visual size

--- Game/Block.lua
+++ Game/Block.lua
@@ -1,11 +1,12 @@
-require("Utility.BlockTypeEnum")
-require("Game.MapInfo")
+require('Utility.BlockTypeEnum')
+require('Game.MapInfo')
 
 local Block_info = {
-	map_info = {},
-	shape = {},
-	body = {},
-	move = {},
+  map_info = {},
+  shape = {},
+  body = {},
+  move = {},
+  visual_points = {},
 }
 Block_info.__index = Block_info
 
@@ -15,119 +16,139 @@ local BLOCK_ROTATE_SPEED = 1
 local BLOCK_MOVE_SPEED = 25
 
 function Block_info.new(map_info, x, y, offset_x, offset_y)
-	-- Set body type
-	local body_type = "kinematic"
-	if
-		map_info.type.r == BLOCK_TYPE.STATIC_BLOCK.r
-		or map_info.type.r == BLOCK_TYPE.BOUNCE_BLOCK.r
-		or map_info.type.r == BLOCK_TYPE.END.r
-	then
-		body_type = "static"
-	end
+  -- Set body type
+  local body_type = 'kinematic'
+  if map_info.type.r == BLOCK_TYPE.STATIC_BLOCK.r or map_info.type.r == BLOCK_TYPE.BOUNCE_BLOCK.r or map_info.type.r == BLOCK_TYPE.END.r then
+    body_type = 'static'
+  end
 
-	-- set block center
-	local center_x = (x - (map_info.width / 2)) * BLOCK_SIZE
-	local center_y = (y - (map_info.height / 2)) * BLOCK_SIZE
+  local scale_offset = 1
+  local visual_points = nil
 
-	-- set physics body variables
-	local body = love.physics.newBody(World, offset_x + center_x, offset_y + center_y, body_type)
-	local shape = love.physics.newRectangleShape(map_info.width * BLOCK_SIZE, map_info.height * BLOCK_SIZE)
-	local fixture = love.physics.newFixture(body, shape)
-	fixture:setCategory(LAYERS.LEVEL)
-	fixture:setFriction(0.7)
+  -- Make end gate collision smaller
+  if map_info.type.r == BLOCK_TYPE.END.r then
+    map_info.height = 2
+    scale_offset = 0.25
 
-	-- check if block is bouncy
-	if
-		map_info.type == BLOCK_TYPE.BOUNCE_BLOCK
-		or map_info.type == BLOCK_TYPE.ROTATING_BOUNCE
-		or map_info.type == BLOCK_TYPE.VERTICAL_BOUNCE
-		or map_info.type == BLOCK_TYPE.HORIZONTAL_BOUNCE
-	then
-		fixture:setRestitution(0.9)
-	end
+    local visual_width_point = map_info.width * BLOCK_SIZE * 0.5
+    local visual_height_point = map_info.height * BLOCK_SIZE * 0.5
+    visual_points = {
+      -visual_width_point,
+      visual_height_point,
+      visual_width_point,
+      visual_height_point,
+      visual_width_point,
+      -visual_height_point,
+      -visual_width_point,
+      -visual_height_point,
+    }
+  end
 
-	-- Set userdata based of block
-	if map_info.type == BLOCK_TYPE.END then
-		-- fixture:setSensor(true)
-		fixture:setCategory(LAYERS.TRIGGER)
-		fixture:setUserData({ entity = "block", part = "end" })
-	elseif map_info.type == BLOCK_TYPE.START then
-		fixture:setUserData({ entity = "block", part = "start" })
-	elseif
-		map_info.type == BLOCK_TYPE.VERTICAL_BLOCK
-		or map_info.type == BLOCK_TYPE.HORIZONTAL_BLOCK
-		or map_info.type == BLOCK_TYPE.STATIC_BLOCK
-		or map_info.type == BLOCK_TYPE.ROTATING_BLOCK
-	then
-		fixture:setUserData({ entity = "block", part = "solid" })
-	elseif
-		map_info.type == BLOCK_TYPE.BOUNCE_BLOCK
-		or map_info.type == BLOCK_TYPE.ROTATING_BOUNCE
-		or map_info.type == BLOCK_TYPE.VERTICAL_BOUNCE
-		or map_info.type == BLOCK_TYPE.HORIZONTAL_BOUNCE
-	then
-		fixture:setUserData({ entity = "block", part = "bounce" })
-	end
+  -- set block center
+  local center_x = (x - (map_info.width / 2)) * BLOCK_SIZE
+  local center_y = (y - (map_info.height / 2)) * BLOCK_SIZE
 
-	-- set specific move type function
-	local move = nil
+  -- set physics body variables
+  local body = love.physics.newBody(World, offset_x + center_x, offset_y + center_y, body_type)
+  local shape = love.physics.newRectangleShape(map_info.width * BLOCK_SIZE * scale_offset, map_info.height * BLOCK_SIZE * scale_offset)
+  local fixture = love.physics.newFixture(body, shape)
+  fixture:setCategory(LAYERS.LEVEL)
+  fixture:setFriction(0.7)
 
-	if body_type == "kinematic" then
-		local is_go_to = true
-		-- Set for vertical movement
-		if map_info.type == BLOCK_TYPE.VERTICAL_BLOCK or map_info.type == BLOCK_TYPE.VERTICAL_BOUNCE then
-			local first = body:getY()
-			local last = first - (BLOCK_MOVE_DISTANCE * BLOCK_SIZE)
-			move = function()
-				if is_go_to then
-					if body:getY() <= last then
-						is_go_to = false
-					end
-					body:setLinearVelocity(0, -BLOCK_MOVE_SPEED)
-				else
-					if body:getY() >= first then
-						is_go_to = true
-					end
-					body:setLinearVelocity(0, BLOCK_MOVE_SPEED)
-				end
-			end
-		-- Set for horizontal movement
-		elseif map_info.type == BLOCK_TYPE.HORIZONTAL_BLOCK or map_info.type == BLOCK_TYPE.HORIZONTAL_BOUNCE then
-			local first = body:getX()
-			local last = first + (BLOCK_MOVE_DISTANCE * BLOCK_SIZE)
-			move = function()
-				if is_go_to then
-					if body:getX() >= last then
-						is_go_to = false
-					end
-					body:setLinearVelocity(BLOCK_MOVE_SPEED, 0)
-				else
-					if body:getX() <= first then
-						is_go_to = true
-					end
-					body:setLinearVelocity(-BLOCK_MOVE_SPEED, 0)
-				end
-			end
-		-- Set for rotating block
-		elseif map_info.type == BLOCK_TYPE.ROTATING_BLOCK or map_info.type == BLOCK_TYPE.ROTATING_BOUNCE then
-			move = function(self)
-				self.body:setAngularVelocity(BLOCK_ROTATE_SPEED)
-			end
-		end
-	end
+  -- check if block is bouncy
+  if
+    map_info.type == BLOCK_TYPE.BOUNCE_BLOCK
+    or map_info.type == BLOCK_TYPE.ROTATING_BOUNCE
+    or map_info.type == BLOCK_TYPE.VERTICAL_BOUNCE
+    or map_info.type == BLOCK_TYPE.HORIZONTAL_BOUNCE
+  then
+    fixture:setRestitution(0.9)
+  end
 
-	return setmetatable({
-		map_info = map_info or map_info.new(),
-		shape = shape,
-		body = body,
-		move = move,
-	}, Block_info)
+  -- Set userdata based of block
+  if map_info.type == BLOCK_TYPE.END then
+    -- fixture:setSensor(true)
+    fixture:setCategory(LAYERS.TRIGGER)
+    fixture:setUserData({ entity = 'block', part = 'end' })
+  elseif map_info.type == BLOCK_TYPE.START then
+    fixture:setUserData({ entity = 'block', part = 'start' })
+  elseif
+    map_info.type == BLOCK_TYPE.VERTICAL_BLOCK
+    or map_info.type == BLOCK_TYPE.HORIZONTAL_BLOCK
+    or map_info.type == BLOCK_TYPE.STATIC_BLOCK
+    or map_info.type == BLOCK_TYPE.ROTATING_BLOCK
+  then
+    fixture:setUserData({ entity = 'block', part = 'solid' })
+  elseif
+    map_info.type == BLOCK_TYPE.BOUNCE_BLOCK
+    or map_info.type == BLOCK_TYPE.ROTATING_BOUNCE
+    or map_info.type == BLOCK_TYPE.VERTICAL_BOUNCE
+    or map_info.type == BLOCK_TYPE.HORIZONTAL_BOUNCE
+  then
+    fixture:setUserData({ entity = 'block', part = 'bounce' })
+  end
+
+  -- set specific move type function
+  local move = nil
+
+  if body_type == 'kinematic' then
+    local is_go_to = true
+    -- Set for vertical movement
+    if map_info.type == BLOCK_TYPE.VERTICAL_BLOCK or map_info.type == BLOCK_TYPE.VERTICAL_BOUNCE then
+      local first = body:getY()
+      local last = first - (BLOCK_MOVE_DISTANCE * BLOCK_SIZE)
+      move = function()
+        if is_go_to then
+          if body:getY() <= last then
+            is_go_to = false
+          end
+          body:setLinearVelocity(0, -BLOCK_MOVE_SPEED)
+        else
+          if body:getY() >= first then
+            is_go_to = true
+          end
+          body:setLinearVelocity(0, BLOCK_MOVE_SPEED)
+        end
+      end
+    -- Set for horizontal movement
+    elseif map_info.type == BLOCK_TYPE.HORIZONTAL_BLOCK or map_info.type == BLOCK_TYPE.HORIZONTAL_BOUNCE then
+      local first = body:getX()
+      local last = first + (BLOCK_MOVE_DISTANCE * BLOCK_SIZE)
+      move = function()
+        if is_go_to then
+          if body:getX() >= last then
+            is_go_to = false
+          end
+          body:setLinearVelocity(BLOCK_MOVE_SPEED, 0)
+        else
+          if body:getX() <= first then
+            is_go_to = true
+          end
+          body:setLinearVelocity(-BLOCK_MOVE_SPEED, 0)
+        end
+      end
+    -- Set for rotating block
+    elseif map_info.type == BLOCK_TYPE.ROTATING_BLOCK or map_info.type == BLOCK_TYPE.ROTATING_BOUNCE then
+      move = function(self)
+        self.body:setAngularVelocity(BLOCK_ROTATE_SPEED)
+      end
+    end
+  end
+
+  return setmetatable({
+    map_info = map_info or map_info.new(),
+    shape = shape,
+    body = body,
+    move = move,
+    visual_points = visual_points,
+  }, Block_info)
 end
 
 function Block_info:unload()
-	self.map_info = nil
-	self.body = nil
-	self.move = nil
+  self.map_info = nil
+  self.body = nil
+  self.move = nil
+  self.visual_points = nil
 end
 
 return Block_info
--- Game/TileMap.lua
+++ Game/TileMap.lua
@@ -1,157 +1,163 @@
-local block_info = require("Game.Block")
-local map_info = require("Game.MapInfo")
-require("Utility.BlockTypeEnum")
-require("Utility.ColorEnum")
-local love = require("love")
+local block_info = require('Game.Block')
+local map_info = require('Game.MapInfo')
+require('Utility.BlockTypeEnum')
+require('Utility.ColorEnum')
+local love = require('love')
 
 local Tilemap = {
-	map = {},
-	static_blocks = {},
-	movable_blocks = {},
-	start_x = 0,
-	start_y = 0,
-	player_offset_x = 0,
-	player_offset_y = 0,
+  map = {},
+  static_blocks = {},
+  movable_blocks = {},
+  start_x = 0,
+  start_y = 0,
+  player_offset_x = 0,
+  player_offset_y = 0,
 }
 Tilemap.__index = Tilemap
 
 function Tilemap.new(player_width, player_height)
-	local player_offset_x = player_width / 2
-	local player_offset_y = player_height / 2
-	return setmetatable({ player_offset_x = player_offset_x or 0, player_offset_y = player_offset_y or 0 }, Tilemap)
+  local player_offset_x = player_width / 2
+  local player_offset_y = player_height / 2
+  return setmetatable({ player_offset_x = player_offset_x or 0, player_offset_y = player_offset_y or 0 }, Tilemap)
 end
 
 function Tilemap:create_map(image)
-	local level = love.image.newImageData(image)
+  local level = love.image.newImageData(image)
 
-	for x = 1, level:getWidth() do
-		self.map[x] = {}
-		for y = 1, level:getHeight() do
-			local r, g, b = love.math.colorToBytes(level:getPixel(x - 1, y - 1))
-			local type = self.has_block_value(r, g, b)
+  for x = 1, level:getWidth() do
+    self.map[x] = {}
+    for y = 1, level:getHeight() do
+      local r, g, b = love.math.colorToBytes(level:getPixel(x - 1, y - 1))
+      local type = self.has_block_value(r, g, b)
 
-			--check if tile is start tile
-			if BLOCK_TYPE.START:has_values(r, g, b) then
-				self.start_x = -x
-				self.start_y = -y
-			elseif type ~= nil then
-				self.map[x][y] = map_info.new(type)
-			end
-		end
-	end
+      --check if tile is start tile
+      if BLOCK_TYPE.START:has_values(r, g, b) then
+        self.start_x = -x
+        self.start_y = -y
+      elseif type ~= nil then
+        self.map[x][y] = map_info.new(type)
+      end
+    end
+  end
 end
 
 function Tilemap:optimize_map()
-	--greedy mesh vertical
-	--check if top block is the same as current
-	--if yes then combine
-	for x, _ in pairs(self.map) do
-		for y, _ in pairs(self.map[x]) do
-			if self.map[x][y - 1] ~= nil and self.map[x][y].type == self.map[x][y - 1].type then
-				self.map[x][y].height = self.map[x][y].height + self.map[x][y - 1].height
-				self.map[x][y - 1] = nil
-			end
-		end
-	end
+  --greedy mesh vertical
+  --check if top block is the same as current
+  --if yes then combine
+  for x, _ in pairs(self.map) do
+    for y, _ in pairs(self.map[x]) do
+      if self.map[x][y - 1] ~= nil and self.map[x][y].type == self.map[x][y - 1].type then
+        self.map[x][y].height = self.map[x][y].height + self.map[x][y - 1].height
+        self.map[x][y - 1] = nil
+      end
+    end
+  end
 
-	--greedy mesh horizontal
-	--check if left mesh is same as current
-	--if yes then combine
-	for x = 2, #self.map do
-		for y, block in pairs(self.map[x]) do
-			if
-				self.map[x - 1][y] ~= nil
-				and block.height == self.map[x - 1][y].height
-				and block.type == self.map[x - 1][y].type
-			then
-				block.width = block.width + self.map[x - 1][y].width
-				self.map[x - 1][y] = nil
-			end
-		end
-	end
+  --greedy mesh horizontal
+  --check if left mesh is same as current
+  --if yes then combine
+  for x = 2, #self.map do
+    for y, block in pairs(self.map[x]) do
+      if self.map[x - 1][y] ~= nil and block.height == self.map[x - 1][y].height and block.type == self.map[x - 1][y].type then
+        block.width = block.width + self.map[x - 1][y].width
+        self.map[x - 1][y] = nil
+      end
+    end
+  end
 end
 
 function Tilemap:load_map()
-	self:optimize_map()
+  self:optimize_map()
 
-	--offset map so player at start tile
-	local start_offset_x = self.start_x * BLOCK_SIZE + self.player_offset_x
-	local start_offset_y = self.start_y * BLOCK_SIZE + self.player_offset_y
+  --offset map so player at start tile
+  local start_offset_x = self.start_x * BLOCK_SIZE + self.player_offset_x
+  local start_offset_y = self.start_y * BLOCK_SIZE + self.player_offset_y
 
-	for x, column in pairs(self.map) do
-		for y, info in pairs(column) do
-			local block = block_info.new(info, x, y, start_offset_x, start_offset_y)
+  for x, column in pairs(self.map) do
+    for y, info in pairs(column) do
+      local block = block_info.new(info, x, y, start_offset_x, start_offset_y)
 
-			--check if block is static or kinematic
-			if block.body:getType() == "kinematic" then
-				table.insert(self.movable_blocks, block)
-			else
-				table.insert(self.static_blocks, block)
-			end
+      --check if block is static or kinematic
+      if block.body:getType() == 'kinematic' then
+        table.insert(self.movable_blocks, block)
+      else
+        table.insert(self.static_blocks, block)
+      end
 
-			self.map[x][y] = nil
-		end
-	end
+      self.map[x][y] = nil
+    end
+  end
 end
 
 function Tilemap:update(_)
-	for _, block in ipairs(self.movable_blocks) do
-		block:move()
-	end
+  for _, block in ipairs(self.movable_blocks) do
+    block:move()
+  end
 end
 
 function Tilemap:draw_map()
-	love.graphics.setLineWidth(5)
+  love.graphics.setLineWidth(5)
 
-	--draw static blocks
-	for _, block in ipairs(self.static_blocks) do
-		love.graphics.setColor(block.map_info.type:rgb_color())
-		love.graphics.polygon("line", block.body:getWorldPoints(block.shape:getPoints()))
-	end
+  --draw static blocks
+  for _, block in ipairs(self.static_blocks) do
+    love.graphics.setColor(block.map_info.type:rgb_color())
 
-	--draw kinematic blocks
-	for _, block in ipairs(self.movable_blocks) do
-		love.graphics.setColor(block.map_info.type:rgb_color())
-		love.graphics.polygon("line", block.body:getWorldPoints(block.shape:getPoints()))
-	end
+    if block.map_info.type.r == BLOCK_TYPE.END.r then
+      -- love.graphics.polygon('line', block.body:getWorldPoints(block.visual_points))
+      love.graphics.push()
+      love.graphics.translate(block.body:getX(), block.body:getY())
+      -- love.graphics.rotate(block.body:getAngle())
+      love.graphics.polygon('line', block.visual_points)
+      love.graphics.pop()
+    else
+      love.graphics.polygon('line', block.body:getWorldPoints(block.shape:getPoints()))
+    end
+  end
+
+  --draw kinematic blocks
+  for _, block in ipairs(self.movable_blocks) do
+    love.graphics.setColor(block.map_info.type:rgb_color())
+    love.graphics.polygon('line', block.body:getWorldPoints(block.shape:getPoints()))
+  end
 end
 
 function Tilemap:unload()
-	-- Unload Map
-	for y, row in pairs(self.map) do
-		for x, _ in pairs(row) do
-			self.map[y][x] = nil
-		end
-		self.map[y] = nil
-	end
+  -- Unload Map
+  for y, row in pairs(self.map) do
+    for x, _ in pairs(row) do
+      self.map[y][x] = nil
+    end
+    self.map[y] = nil
+  end
 
-	-- Unload physic blocks
-	for x, block in pairs(self.static_blocks) do
-		if block.body then
-			block:unload()
-			self.static_blocks[x] = nil
-		end
-	end
+  -- Unload physic blocks
+  for x, block in pairs(self.static_blocks) do
+    if block.body then
+      block:unload()
+      self.static_blocks[x] = nil
+    end
+  end
 
-	for x, block in pairs(self.movable_blocks) do
-		if block.body then
-			block:unload()
-			self.movable_blocks[x] = nil
-		end
-	end
+  for x, block in pairs(self.movable_blocks) do
+    if block.body then
+      block:unload()
+      self.movable_blocks[x] = nil
+    end
+  end
 end
 
 function Tilemap.has_block_value(r, g, b)
-	for _, type in pairs(BLOCK_TYPE) do
-		if type:has_values(r, g, b) then
-			return type
-		end
-	end
-	return nil
+  for _, type in pairs(BLOCK_TYPE) do
+    if type:has_values(r, g, b) then
+      return type
+    end
+  end
+  return nil
 end
 
 function Tilemap:get_spawnpoint()
-	return self.start_x, self.start_y
+  return self.start_x, self.start_y
 end
 
 return Tilemap
